e6bbcfc02836eaddbb2be863c60d24f6b1532e6d
Erik Agujari <erikagujari@gmail.com>
2021-10-23 13:58:33 +0200

    - remove `Main.storyboard` in benefit of a specific `HomeViewController`

---
+++ marvel-heroes/marvel-heroes/App/Home/HomeViewController.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewController.swift
+//  marvel-heroes
+//
+//  Created by Erik Agujari on 23/10/21.
+//
+
+import UIKit
+
+final class HomeViewController: UITableViewController {
+    
+}

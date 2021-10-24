cdf3a1cbc9f9acfd556a100b5ca4a98307860f19
Erik Agujari <erikagujari@gmail.com>
2021-10-24 11:15:10 +0200

    - use expect helper method on `FetchCharactersUseCaseTests`

--- marvel-heroes/marvel-heroesTests/FetchCharactersUseCaseTests.swift
+++ marvel-heroes/marvel-heroesTests/FetchCharactersUseCaseTests.swift
@@ -12,71 +12,43 @@ final class FetchCharacterUseCaseTests: XCTestCase {
     func test_executeFails_onRepositoryError() {
         let error = MarvelError.serviceError
         let sut = makeSUT(result: Fail<[MarvelCharacter], MarvelError>(error: error).eraseToAnyPublisher())
-        let exp = expectation(description: "Waiting for execute to complete")
-        var cancellables = Set<AnyCancellable>()
-        sut.execute(limit: 0, offset: 1)
-            .sink { result in
-                switch result {
-                case let .failure(receivedError):
-                    XCTAssertEqual(error, receivedError)
-                case .finished:
-                    XCTFail("It should not finish")
-                }
-                exp.fulfill()
-            } receiveValue: { _ in
-                
-            }
-            .store(in: &cancellables)
-
-        wait(for: [exp], timeout: 0.1)
+        
+        expect(sut: sut, endsWithResult: .failure(error))
     }
     
     func test_executeFails_onRepositoryEmptyList() {
         let sut = makeSUT(result: Just<[MarvelCharacter]>([]).setFailureType(to: MarvelError.self).eraseToAnyPublisher())
-        let exp = expectation(description: "Waiting for execute to complete")
-        var cancellables = Set<AnyCancellable>()
-        sut.execute(limit: 0, offset: 1)
-            .sink { result in
-                switch result {
-                case let .failure(receivedError):
-                    XCTAssertEqual(.serviceError, receivedError)
-                case .finished:
-                    XCTFail("It should not finish")
-                }
-                exp.fulfill()
-            } receiveValue: { _ in
-                
-            }
-            .store(in: &cancellables)
-
-        wait(for: [exp], timeout: 0.1)
+        
+        expect(sut: sut, endsWithResult: .failure(.serviceError))
     }
     
     func test_executeFinishes_onRepositoryNotEmptyList() {
         let characters = [MarvelCharacter(id: 0, name: "", description: "", modified: "", thumbnail: Thumbnail(path: "", fileExtension: ""))]
         let sut = makeSUT(result: Just<[MarvelCharacter]>(characters).setFailureType(to: MarvelError.self).eraseToAnyPublisher())
-        let exp = expectation(description: "Waiting for execute to complete")
-        var cancellables = Set<AnyCancellable>()
-        sut.execute(limit: 0, offset: 1)
-            .sink { result in
-                switch result {
-                case .failure:
-                    XCTFail("It should not finish on failure")
-                case .finished: break
-                }
-                exp.fulfill()
-            } receiveValue: { _ in
-                
-            }
-            .store(in: &cancellables)
-
-        wait(for: [exp], timeout: 0.1)
+        
+        expect(sut: sut, endsWithResult: .finished)
     }
 }
 
 private extension FetchCharacterUseCaseTests {
     func makeSUT(result: AnyPublisher<[MarvelCharacter], MarvelError>) -> FetchCharacterUseCase {
         return FetchCharacterUseCaseProvider(repository: CharacterRepositoryStub(result: result))
+    }
+    
+    func expect(sut: FetchCharacterUseCase, endsWithResult expectedResult: Subscribers.Completion<MarvelError>, file: StaticString = #filePath, line: UInt = #line) {
+        let exp = expectation(description: "Waiting to complete execute")
+        var cancellables = Set<AnyCancellable>()
+        
+        sut.execute(limit: 0, offset: 0)
+            .sink { receivedResult in
+                XCTAssertEqual(expectedResult, receivedResult, file: file, line: line)
+                exp.fulfill()
+            } receiveValue: { characters in
+                XCTAssertFalse(characters.isEmpty)
+            }
+            .store(in: &cancellables)
+        
+        wait(for: [exp], timeout: 0.1)
     }
     
     struct CharacterRepositoryStub: CharacterRepository {

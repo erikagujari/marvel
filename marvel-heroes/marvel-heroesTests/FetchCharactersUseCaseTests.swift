d66cc231fba1cfac18120052253341bf5cf39121
Erik Agujari <erikagujari@gmail.com>
2021-10-24 11:11:22 +0200

    - move `FetchCharacterUseCase` to production code

--- marvel-heroes/marvel-heroesTests/FetchCharactersUseCaseTests.swift
+++ marvel-heroes/marvel-heroesTests/FetchCharactersUseCaseTests.swift
@@ -91,27 +91,3 @@ private extension FetchCharacterUseCaseTests {
         }
     }
 }
-
-protocol FetchCharacterUseCase {
-    func execute(limit: Int, offset: Int) -> AnyPublisher<[MarvelCharacter], MarvelError>
-}
-
-struct FetchCharacterUseCaseProvider: FetchCharacterUseCase {
-    private let repository: CharacterRepository
-    
-    init(repository: CharacterRepository) {
-        self.repository = repository
-    }
-    
-    func execute(limit: Int, offset: Int) -> AnyPublisher<[MarvelCharacter], MarvelError> {
-        return repository.fetch(limit: limit, offset: offset)
-            .flatMap { characters -> AnyPublisher<[MarvelCharacter], MarvelError> in
-                guard characters.isEmpty else {
-                    return Just<[MarvelCharacter]>(characters).setFailureType(to: MarvelError.self).eraseToAnyPublisher()
-                }
-                
-                return Fail<[MarvelCharacter], MarvelError>(error: MarvelError.serviceError).eraseToAnyPublisher()
-            }
-            .eraseToAnyPublisher()
-    }
-}
